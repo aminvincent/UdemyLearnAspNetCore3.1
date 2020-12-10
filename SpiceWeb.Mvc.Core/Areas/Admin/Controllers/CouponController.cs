07462ae0b29ba681b6acfd8df4d27b189d85b8be
Amin Vincent <aminvincent@ignitech.com>
2020-12-10 15:19:26 +0700

    add Coupon menu with Create View

---
+++ SpiceWeb.Mvc.Core/Areas/Admin/Controllers/CouponController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SpiceWeb.Mvc.Core.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpiceWeb.Mvc.Core.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CouponController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CouponController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _db.Coupon.ToListAsync());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
