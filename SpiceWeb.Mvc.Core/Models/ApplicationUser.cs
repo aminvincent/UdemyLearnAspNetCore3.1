5f9206200fb477340c76b0dbf4028958f9b0f3f5
Amin Vincent <aminvincent@ignitech.com>
2020-12-28 14:46:03 +0700

    adding scafold Account identity User

---
+++ SpiceWeb.Mvc.Core/Models/ApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpiceWeb.Mvc.Core.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Name { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int PostalCode { get; set; }
+    }
+}
